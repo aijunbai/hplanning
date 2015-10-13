c80a25f39bd8a6a7a27a11ea525b9100597b9e84
Aijun Bai <aijunbai@gmail.com>
2015-10-13 13:56:30 -0700

    updated test.sh

--- d2ng-pomcp/test.sh
+++ d2ng-pomcp/test.sh
@@ -3,23 +3,13 @@
 sh ./clear.sh
 make
 
-#./run.sh -p rooms_0 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 &
-#./run.sh -p rooms_1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 &
+./run.sh -p redundant_object_0 -m -1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_0 -m 1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_1 -m -1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10
+./run.sh -p redundant_object_1 -m 1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_1 -m 2 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_1 -m 4 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_1 -m 8 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_1 -m 16 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
+./run.sh -p redundant_object_1 -m 32 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 
 
-./run.sh -p redundant_object_0 -m -1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-./run.sh -p redundant_object_0 -m 1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-./run.sh -p redundant_object_1 -m -1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-
-wait
-
-./run.sh -p redundant_object_1 -m 1 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-./run.sh -p redundant_object_1 -m 2 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-./run.sh -p redundant_object_1 -m 4 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-
-wait
-
-./run.sh -p redundant_object_1 -m 8 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-./run.sh -p redundant_object_1 -m 16 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-./run.sh -p redundant_object_1 -m 32 -h 12 -v 0 -S 1 -P 0 -u 0 -t 1 -r 2 -R 100 -L 0 -H 10 #&
-
-wait

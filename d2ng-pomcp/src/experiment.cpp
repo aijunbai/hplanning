68a56e0042b10f617839733630bf5c0ae0ca3a5b
Aijun Bai <aijunbai@gmail.com>
2015-10-25 20:34:38 -0700

    refined exp output

--- d2ng-pomcp/src/experiment.cpp
+++ d2ng-pomcp/src/experiment.cpp
@@ -6,34 +6,26 @@
 using namespace std;
 
 EXPERIMENT::PARAMS::PARAMS()
-    : NumRuns(1000),
-      NumSteps(100000),
-      TimeOut(3600),
-      MinDoubles(0),
-      MaxDoubles(20),
-      TransformDoubles(-4),
-      TransformAttempts(1000),
-      Accuracy(0.001),
-      UndiscountedHorizon(1000) {}
+    : NumRuns(1000), NumSteps(100000), TimeOut(3600), MinDoubles(0),
+      MaxDoubles(20), TransformDoubles(-4), TransformAttempts(1000),
+      Accuracy(0.001), UndiscountedHorizon(1000) {}
 
 EXPERIMENT::EXPERIMENT(const SIMULATOR &real, const SIMULATOR &simulator,
                        const string &outputFile, EXPERIMENT::PARAMS &expParams,
                        MCTS::PARAMS &searchParams)
-    : Real(real),
-      Simulator(simulator),
-      ExpParams(expParams),
+    : Real(real), Simulator(simulator), ExpParams(expParams),
       SearchParams(searchParams),
-      OutputFile(outputFile.c_str(), fstream::out | fstream::app)
-{
+      OutputFile(outputFile.c_str(), fstream::out | fstream::app) {
   SearchParams.ExplorationConstant = simulator.GetRewardRange();
   MCTS::InitFastUCB(SearchParams.ExplorationConstant); //初始化 UCB 表格缓存
 }
 
 void EXPERIMENT::Run() {
   boost::timer timer;
-  MCTS *mcts = Real.mHierarchicalPlanning?
-        safe_cast<MCTS*>(new HierarchicalMCTS(Simulator, SearchParams)):
-        safe_cast<MCTS*>(new FlatMCTS(Simulator, SearchParams));
+  MCTS *mcts =
+      Real.mHierarchicalPlanning
+          ? safe_cast<MCTS *>(new HierarchicalMCTS(Simulator, SearchParams))
+          : safe_cast<MCTS *>(new FlatMCTS(Simulator, SearchParams));
 
   double undiscountedReturn = 0.0;
   double discountedReturn = 0.0;
@@ -42,8 +34,9 @@ void EXPERIMENT::Run() {
   bool outOfParticles = false;
   int t = 0;
 
-  STATE *state = Real.CreateStartState();  //真实的世界状态
-  if (SearchParams.Verbose >= 1) Real.DisplayState(*state, cout);
+  STATE *state = Real.CreateStartState(); //真实的世界状态
+  if (SearchParams.Verbose >= 1)
+    Real.DisplayState(*state, cout);
 
   for (t = 0; t < ExpParams.NumSteps; t++) {
     int observation;
@@ -51,12 +44,12 @@ void EXPERIMENT::Run() {
 
     boost::timer timer_per_action;
     mcts->Search();
-    int action = mcts->SelectAction();  // 用 Monte Carlo 方法选择一个动作
+    int action = mcts->SelectAction(); // 用 Monte Carlo 方法选择一个动作
     Results.TimePerAction.Add(timer_per_action.elapsed());
 
     terminal = Real.Step(
         *state, action, observation,
-        reward);  //根据 state 和 action 转移到下一个状态，获得实际观察和回报
+        reward); //根据 state 和 action 转移到下一个状态，获得实际观察和回报
     Results.Reward.Add(reward);
     undiscountedReturn += reward;
     discountedReturn += reward * discount;
@@ -79,10 +72,12 @@ void EXPERIMENT::Run() {
       break;
     }
 
-    outOfParticles = !mcts->Update(action, observation, reward, *state);  //更新历史信息，得到新的 Root 节点，设置好初始信念状态
+    outOfParticles = !mcts->Update(
+        action, observation, reward,
+        *state); //更新历史信息，得到新的 Root 节点，设置好初始信念状态
     if (outOfParticles) {
       assert(!Real.mHierarchicalPlanning);
-      break;  // Out of particles, finishing episode with SelectRandom
+      break; // Out of particles, finishing episode with SelectRandom
     }
 
     if (timer.elapsed() > ExpParams.TimeOut) {
@@ -92,7 +87,7 @@ void EXPERIMENT::Run() {
     }
   }
 
-  if (outOfParticles)  //特殊情况处理
+  if (outOfParticles) //特殊情况处理
   {
     cout << "Out of particles, finishing episode with SelectRandom" << endl;
     while (++t < ExpParams.NumSteps) {
@@ -140,7 +135,7 @@ void EXPERIMENT::Run() {
 }
 
 void EXPERIMENT::MultiRun() {
-  for (int n = 0; n < ExpParams.NumRuns; n++)  //实验次数
+  for (int n = 0; n < ExpParams.NumRuns; n++) //实验次数
   {
     cout << "Starting run " << n + 1 << " with " << SearchParams.NumSimulations
          << " simulations... " << endl;
@@ -158,26 +153,27 @@ void EXPERIMENT::MultiRun() {
 
 void EXPERIMENT::DiscountedReturn() {
   cout << "Main runs" << endl;
-  OutputFile << "#Simulations\tRuns\tUndiscounted return\tUndiscounted error\t"
-                "Discounted return\tDiscounted error\t"
-                "Time\tTimePerAction\tExploredNodes\tExploredDepth\n";
+  OutputFile << "#Simulations\tRuns\tUndiscountedReturn\tUndiscountedError\t"
+                "DiscountedReturn\tDiscountedError\t"
+                "Time\tTimePerAction\tExploredNodes\tExploredNodesError\t"
+                "ExploredDepth\tExploredDepthError\n";
 
   SearchParams.MaxDepth = Simulator.GetHorizon(
       ExpParams.Accuracy,
-      ExpParams.UndiscountedHorizon);  //搜索过程中的最大深度
+      ExpParams.UndiscountedHorizon); //搜索过程中的最大深度
   ExpParams.NumSteps = Real.GetHorizon(
-      ExpParams.Accuracy, ExpParams.UndiscountedHorizon);  //实验的最大步长
+      ExpParams.Accuracy, ExpParams.UndiscountedHorizon); //实验的最大步长
 
   for (int i = ExpParams.MinDoubles; i <= ExpParams.MaxDoubles; i++) {
-    SearchParams.NumSimulations = 1 << i;  //迭代次数 iterations
+    SearchParams.NumSimulations = 1 << i; //迭代次数 iterations
 
-    if (SearchParams.TimeOutPerAction < 0.0) {  //非anytime模式
-      SearchParams.NumStartStates = 1 << i;     //初始粒子数
+    if (SearchParams.TimeOutPerAction < 0.0) { //非anytime模式
+      SearchParams.NumStartStates = 1 << i;    //初始粒子数
     }
 
     if (i + ExpParams.TransformDoubles >= 0)
       SearchParams.NumTransforms = 1
-                                   << (i + ExpParams.TransformDoubles);  // 1/16
+                                   << (i + ExpParams.TransformDoubles); // 1/16
     else
       SearchParams.NumTransforms = 1;
     SearchParams.MaxAttempts =
@@ -194,10 +190,10 @@ void EXPERIMENT::DiscountedReturn() {
          << " +- " << Results.DiscountedReturn.GetStdErr() << endl
          << "#Time = " << Results.Time.GetMean() << endl
          << "#TimePerAction = " << Results.TimePerAction.GetMean() << endl
-         << "#ExploredNodes = " << Results.ExploredNodes.GetMean()
-         << " +- " << Results.ExploredNodes.GetStdErr() << endl
-         << "#ExploredDepth = " << Results.ExploredDepth.GetMean()
-         << " +- " << Results.ExploredDepth.GetStdErr() << endl;
+         << "#ExploredNodes = " << Results.ExploredNodes.GetMean() << " +- "
+         << Results.ExploredNodes.GetStdErr() << endl
+         << "#ExploredDepth = " << Results.ExploredDepth.GetMean() << " +- "
+         << Results.ExploredDepth.GetStdErr() << endl;
 
     OutputFile << SearchParams.NumSimulations << "\t" << Results.Time.GetCount()
                << "\t" << Results.UndiscountedReturn.GetMean() << "\t"
@@ -209,7 +205,6 @@ void EXPERIMENT::DiscountedReturn() {
                << Results.ExploredNodes.GetMean() << "\t"
                << Results.ExploredNodes.GetStdErr() << "\t"
                << Results.ExploredDepth.GetMean() << "\t"
-               << Results.ExploredDepth.GetStdErr() << "\t"
-               << endl;
+               << Results.ExploredDepth.GetStdErr() << "\t" << endl;
   }
 }

3d41c685242ebb1e526b4690865ba6ca5b5323a5
Aijun Bai <aijunbai@gmail.com>
2016-04-17 20:48:25 -0700

    refined run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,23 @@ set -o nounset
 PROBLEM="rocksample"
 PROBLEM="redundant_object_0"
 PROBLEM="redundant_object_1"
-PROBLEM="continousrooms_1"
-PROBLEM="rooms_1"
 PROBLEM="continousrooms_0"
+PROBLEM="continousrooms_1"
 PROBLEM="rooms_0"
+PROBLEM="rooms_1"
 
+MAP="data/10_rooms_blocked.map"
+MAP="data/16_rooms_blocked.map"
+MAP="data/32_rooms_blocked.map"
+MAP="data/4_rooms_blocked.map"
+MAP="data/2_rooms_blocked.map"
 MAP="data/10_rooms.map"
 MAP="data/16_rooms.map"
 MAP="data/32_rooms.map"
 MAP="data/4_rooms.map"
 MAP="data/2_rooms.map"
 MAP="data/8_rooms.map"
+MAP="data/8_rooms_blocked.map"
 
 HPLANNING=1
 ACTIONABSTRACTION=0
